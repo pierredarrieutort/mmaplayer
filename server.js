caf859a459408f230634121ee697e313c00fe53f
Pierre DARRIEUTORT <p.darrieutort@outlook.fr>
2021-01-27 01:04:35 +0100

    obfuscted sensible data

--- server.js
+++ server.js
@@ -68,15 +68,7 @@ const schema = new GraphQLSchema({
     mutation: RootMutation
 })
 
-const
-    dbname = 'mmaplayer',
-    password = 'Cluster0_root'
-
-mongoose.connect(`mongodb+srv://Cluster0_root:${password}@cluster0.bxrm8.mongodb.net/${dbname}?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-})
+var _0xe8c6 = ["\x6D\x6D\x61\x70\x6C\x61\x79\x65\x72", "\x43\x6C\x75\x73\x74\x65\x72\x30\x5F\x72\x6F\x6F\x74", "\x6D\x6F\x6E\x67\x6F\x64\x62\x2B\x73\x72\x76\x3A\x2F\x2F\x43\x6C\x75\x73\x74\x65\x72\x30\x5F\x72\x6F\x6F\x74\x3A", "\x40\x63\x6C\x75\x73\x74\x65\x72\x30\x2E\x62\x78\x72\x6D\x38\x2E\x6D\x6F\x6E\x67\x6F\x64\x62\x2E\x6E\x65\x74\x2F", "\x3F\x72\x65\x74\x72\x79\x57\x72\x69\x74\x65\x73\x3D\x74\x72\x75\x65\x26\x77\x3D\x6D\x61\x6A\x6F\x72\x69\x74\x79", "\x63\x6F\x6E\x6E\x65\x63\x74"]; const dbn = _0xe8c6[0], pas = _0xe8c6[1]; mongoose[_0xe8c6[5]](`${_0xe8c6[2]}${pas}${_0xe8c6[3]}${dbn}${_0xe8c6[4]}`, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 
 app.use(
     '/graphql',
@@ -86,8 +78,8 @@ app.use(
     }),
 )
 
-mongoose.connection.on('connected', () => console.info('Mongoose connected'))
 mongoose.connection.on('error', err => console.error('Mongoose default connection error: ' + err))
+mongoose.connection.on('connected', () => console.info('Mongoose connected'))
 
 app.get('/retrievedata', (req, res) =>
     nodeFetch(`http://localhost:${port}/graphql`, {
@@ -108,6 +100,7 @@ app.get('/retrievedata', (req, res) =>
 
 
 app.get('/redditscrap', (req, res) => res.render('redditScrap'))
+app.get('/findanddeleteduplicates', (req, res) => res.render('FindAndDeleteDuplicates'))
 
 const stringParser = bodyParser.text()
 app.post('/fixunavailablevideo', stringParser, ({ body }, res) =>
