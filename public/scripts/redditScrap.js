7fc176d14296c727cc1e89588e5267ff71fec601
Pierre DARRIEUTORT <p.darrieutort@outlook.fr>
2021-02-07 17:39:25 +0100

    maintenance logic properly enhanced

--- public/scripts/redditScrap.js
+++ public/scripts/redditScrap.js
@@ -1,56 +1,65 @@
-let retrievedData = {};
-
-function scrapUrl(after = '') {
-    fetch(`https://www.reddit.com/domain/mmashare.fullfight.video/new/.json?limit=100&after=${after}`)
-        .then(d => d.json())
-        .then(({ data: dataParent }) => {
-            dataParent.children.forEach(({ data }) => retrievedData[data.url] = [data.title, data.url])
-            retrievedData = Object.values(retrievedData)
-
-            dataParent.after
-                ? scrapUrl(dataParent.after)
-                : startTreatment()
-        })
-}
-
-function startTreatment() {
-    // console.log(retrievedData)
-    retrievedData.reverse().forEach(([title, source], i) => setTimeout(
-        async () => {
-            await feedTreatment(title, source)
-            console.info(`${parseInt((i + 1) / retrievedData.length * 100)}%`)
-        },
-        i
-    ))
-}
-
-async function feedTreatment(title, source) {
-    title = title.replaceAll(/updated/ig, '').replace(':', '').replace(/\s+/g, ' ').trim()
-
-    const responseScrap = await fetch(/*'https://cors-anywhere.herokuapp.com/' + */source)
-    const dataScrap = await responseScrap.text()
-
-    const template = document.createElement('template')
-    template.innerHTML = dataScrap
-
-    source = Array.from(template.content.querySelector('video>source[src]')).map(({ src }) => src)
-
-    if (title && source.length) {
-        await fetch('http://localhost:3000/graphql', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                query: ` mutation {
-                    addEvent(title: "${title}", source: "${source}") {
-                        title
-                        source
-                    }
-                }`
-            })
-        })
+class RedditScrap {
+    constructor() {
+        this.retrievedData = {}
     }
-    // else console.warn(title, 'source error')
+
+    scrapUrl(after = '') {
+        fetch(`https://www.reddit.com/domain/mmashare.fullfight.video/new.json?limit=100&after=${after}`)
+            .then(d => d.json())
+            .then(({ data: dataParent }) => {
+                dataParent.children.forEach(({ data }) => this.retrievedData[data.url] = [data.title, data.url])
+                this.retrievedData = Object.values(this.retrievedData)
+
+                dataParent.after
+                    ? this.scrapUrl(dataParent.after)
+                    : this.startTreatment()
+            })
+    }
+
+    async startTreatment() {
+        // console.log(this.retrievedData)
+
+        for (const [i, [title, source]] of this.retrievedData.reverse().entries()) {
+            await this.feedTreatment(title, source)
+            console.info(`${parseInt((i + 1) / this.retrievedData.length * 100)}%`)
+        }
+    }
+
+    async feedTreatment(title, source) {
+        title = title
+            .replaceAll(/updated/ig, '')
+            .replace(':', '')
+            .replace(/\s+/g, ' ')
+            .trim()
+
+        const responseScrap = await fetch(source)
+
+        if (responseScrap.status !== 404) {
+            const
+                dataScrap = await responseScrap.text(),
+                template = document.createElement('template')
+
+            template.innerHTML = dataScrap
+
+            source = template.content.querySelector('video>source[src]')?.src
+
+            if (title && source) {
+                fetch('http://localhost:3000/graphql', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        query: ` mutation {
+                                    addEvent(title: "${title}", source: "${source}") {
+                                        title
+                                        source
+                                    }
+                                }`
+                    })
+                }).then(() => console.log(`${source} has been added`))
+            }
+        }
+    }
 }
 
 
-document.getElementById('redditScrap').addEventListener('click', scrapUrl)
+document.getElementById('redditScrap').addEventListener('click', () => new RedditScrap().scrapUrl())
